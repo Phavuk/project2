1d57b020c59ec3ded6396dcc7c9e49da1d9ceaa9
Adam966 <adam.ivan@akademiasovy.sk>
2019-02-24 17:16:54 +0100

    add improvements

--- test/test.ino
+++ test/test.ino
@@ -3,50 +3,63 @@
 #include <ArduinoJson.h>
 #include <NTPClient.h>
 #include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 
-#define USE_SERIAL Serial
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
 #define interval 15000
-
-#define waterSurface A0
-
 unsigned long previousMillis = 0;
 
+const char* ssid     = "MI";
+const char* password = "heslo123456789";
+
+//////////////////////////////////////////////// SENSOR PIN SETUP /////////////////////////////////////
+
+#define waterSurface A0
+#define wsPower 1
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
 SocketIoClient webSocket;
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP);
 
+////////////////////////////////////////////////// SETUP /////////////////////////////////////////////
+
 void setup() {
-    USE_SERIAL.begin(115200);
+    Serial.begin(115200);
 
-    USE_SERIAL.setDebugOutput(true);
+    Serial.setDebugOutput(true);
 
-    USE_SERIAL.println();
-    USE_SERIAL.println();
-    USE_SERIAL.println();
+    Serial.println();
+    Serial.println();
+    Serial.println();
 
       for(uint8_t t = 4; t > 0; t--) {
-          USE_SERIAL.printf("[SETUP] BOOT WAIT %d...\n", t);
-          USE_SERIAL.flush();
+          Serial.printf("[SETUP] BOOT WAIT %d...\n", t);
+          Serial.flush();
           delay(1000);
       }
 
     //Connection to WIFI   
-    const char* ssid     = "MI";
-    const char* password = "heslo123456789";
-    
     Serial.print("Connecting to ");
     Serial.println(ssid);
+    WiFi.mode(WIFI_STA);
     WiFi.begin(ssid, password);
-    
+
     while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
-      Serial.print(".");
+        delay(500);
+        Serial.print(".");
     }
 
     Serial.println("");
     Serial.println("WiFi connected.");
     Serial.println("IP address: ");
     Serial.println(WiFi.localIP());
+    
+    //setup OTA
+    ArduinoOTA.setHostname("ESP 32");
+    arduinoOTA();
 
     //establish time client
     timeClient.begin();
@@ -55,13 +68,15 @@ void setup() {
     //establish socketIO connection
     webSocket.begin("192.168.1.109", 5485, "/socket.io/?transport=websocket");
     webSocket.on("pourFlower", pourFlower);
+    webSocket.on("disconnect", disconection);
+    webSocket.on("connect", conection);
+
+    //setup pinMode
+    pinMode(wsPower, OUTPUT); //D4
 }
 
-//function for WIFI reconection
+/////////////////////////////////////////// WIFI reconection /////////////////////////////////////
 void wifiConection() {
- const char* ssid     = "MI";
- const char* password = "heslo123456789";
- 
  if (WiFi.status() != WL_CONNECTED) {
     
     WiFi.begin(ssid, password);
@@ -76,11 +91,47 @@ void wifiConection() {
 
       timeClient.begin();
       timeClient.setTimeOffset(3600);
+      
+      webSocket.begin("192.168.1.109", 5485, "/socket.io/?transport=websocket");
+      webSocket.on("pourFlower", pourFlower);
     }
   }
 }
 
+//////////////////////////////////////////// OTA //////////////////////////////////////////////////
+
+void arduinoOTA() {
+  ArduinoOTA.setHostname("myesp32");
+  
+  ArduinoOTA.onStart([]() {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH)
+        type = "sketch";
+      else // U_SPIFFS
+        type = "filesystem";
+
+      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+      Serial.println("Start updating " + type);
+    })
+    .onEnd([]() {
+      Serial.println("\nEnd");
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    })
+    .onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+      else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    });
+
+  ArduinoOTA.begin();
+}
 ////////////////////////////////////////// MEASURE DATA ////////////////////////////////////////////
+
 void measureData() {
   String id = "kgj45as";
   float temperature = 50; 
@@ -91,10 +142,17 @@ void measureData() {
   createJson(temperature, humidityAir, humiditySoil, waterSurface, id);
 }
 
+//////////////////////////////////////// SENSORS FUNCTIONS ////////////////////////////////////////
+
+//WATER SURFACE
 float getWaterSurface() {
-  return analogRead(waterSurface);
+  digitalWrite(wsPower, HIGH);
+  float ws = analogRead(waterSurface);
+  digitalWrite(wsPower, LOW);
+  return ws;
 }
 
+//DATE
 String getDate() {
   while(!timeClient.update()) {
     timeClient.forceUpdate();
@@ -128,8 +186,6 @@ void createJson(float temperature, float humidityAir, float humiditySoil, float 
     
     JsonObject& date = root.createNestedObject("date");
     date["date"] = getDate();
-
-    root.printTo(Serial);
     
     char output[capacity];
     root.printTo(output);
@@ -140,18 +196,29 @@ void createJson(float temperature, float humidityAir, float humiditySoil, float 
 
 void sendData(char* output) {
     webSocket.emit("arduinoData", output);
-    USE_SERIAL.println("Data sended.");
+    Serial.println("Data sended.");
 }
 
 void pourFlower(const char * payload, size_t length) {
-  USE_SERIAL.println("Data prijate");
+  Serial.println("Pour flower");
+}
+
+void conection(const char * payload, size_t length) {
+  Serial.println("Client connected to server.");
+}
+
+void disconection(const char * payload, size_t length) {
+   Serial.println("Client disconnected from server.");
+   webSocket.begin("192.168.1.109", 5485, "/socket.io/?transport=websocket");
 }
 
 /////////////////////////////////////////////// LOOP ///////////////////////////////////////////
+
 void loop() {
     webSocket.loop();
     wifiConection();
-
+    ArduinoOTA.handle();
+    
     if (millis() - previousMillis >= interval) {
       previousMillis = millis();    
       measureData();
